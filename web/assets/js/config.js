5bc5a62398a82f2bbc6c9c19e9287c18d294b27a
Nunes Jose Churrana <nunstech@gmail.com>
2026-07-22 04:38:59 +0300

    Update config.js

--- web/assets/js/config.js
+++ web/assets/js/config.js
@@ -4,7 +4,7 @@ window.COURSE_PLATFORM_CONFIG = Object.freeze({
   appName: 'Economia e Avaliação de Projetos Industriais',
   organizationName: 'LMTWEBNAIRS',
   publicAppUrl: 'COLE_AQUI_A_URL_DO_GITHUB_PAGES',
-  institutionalUrl: '',
+  institutionalUrl: 'https://lmtwebnairs.com/summer_school_2026',
   supportEmail: '',
   pollIntervalMs: 60000,
   maxImageDimension: 1800,
